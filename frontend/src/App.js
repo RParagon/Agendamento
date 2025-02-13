db3d96a612a87c9ba42c0025ae5e5a3a0aa14bfa
Rafael Paragon <rvirtualmark2@gmail.com>
2025-02-13 08:56:19 +0000

    att app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-// frontend/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ClientForm from './components/ClientForm';
 import AdminDashboard from './components/AdminDashboard';
 
